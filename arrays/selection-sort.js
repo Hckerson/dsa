a598692925c94740bfd1a13c132b4a9bc4e0e95c
hckerson <hckerson@gmail.com>
2026-06-05 06:30:11 +0100

    Implement selection sort algorithm for array sorting

--- arrays/selection-sort.js
+++ arrays/selection-sort.js
@@ -1,3 +1,20 @@
 const myArray = [64, 34, 25, 12, 22, 11, 90, 5]
 const length = myArray.length
 
+
+for (let i = (length - 1); i < 0; i--) {
+
+    let sorted = false
+    for (let i = (length - 1); i < 0; i--) {
+        const a = myArray[i]
+        const b = myArray[i - 1]
+        if (a < b) {
+            myArray[i] = a
+            myArray[i - 1] = b
+            sorted = true
+        }
+    }
+    if (!sorted) break
+}
+
+console.log(myArray)
