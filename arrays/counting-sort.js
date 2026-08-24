2b568c9bbfca6021d40c3ae16c8404aaeb588997
Hckerson <166761968+Hckerson@users.noreply.github.com>
2026-08-24 14:41:03 +0100

    Remove counting sort implementation and clean up quick sort code; retain console logging for myArray

--- arrays/counting-sort.js
+++ arrays/counting-sort.js
@@ -1,25 +1,8 @@
 const myArray = [64, 34, 25, 12, 22, 11, 90, 5, 25, 12, 64];
 const length = myArray.length;
 
-const countSort = (arr) => {
-	const sorted = new Map();
+const x = Array(10).fill()
+x[4] = "my"
+console.log(x)
 
-	for (let i = 0; i <= length - 1; i++) {
-		sorted.set(
-			arr[i],
-			sorted.get(arr[i]) === undefined ? 1 : sorted.get(arr[i]) + 1,
-		);
-	}
-
-	arr.length = 0;
-
-	Array.from(sorted)
-		.sort(([a], [b]) => a - b)
-		.forEach(([a, b]) => {
-			Array(b).fill().forEach(() => arr.push(a));
-		});
-
-	console.log(myArray);
-};
-
-countSort(myArray);
+console.log(myArray);
--- arrays/quick-sort.js
+++ arrays/quick-sort.js
@@ -1,24 +1,6 @@
 const myArray = [64, 34, 25, 12, 22, 11, 90, 5];
 const length = myArray.length;
 
-let unSorted = true;
-let currentlyTrackedIndex = 0;
 
-function rearrange(activeIndex, trackedIndex) {
-	const target = myArray[activeIndex];
-
-	if (myArray[trackedIndex] > target) {
-		myArray.splice(activeIndex, 1);
-		myArray.splice(trackedIndex, 0, target);
-		trackedIndex = trackedIndex + 1;
-	}
-}
-
-while (unSorted) {
-	for (let i = 1; i <= length - 1; i++) {
-		rearrange(i, currentlyTrackedIndex)
-	}
-	
-}
 
 console.log(myArray);
