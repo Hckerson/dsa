e112c84c655d579f3dcf2e9337252cbedbb473d0
hckerson <hckerson@gmail.com>
2026-08-28 18:23:40 +0100

    Refactor radix sort implementation; update extractDigit function and improve sorting logic

--- arrays/radix-sort.js
+++ arrays/radix-sort.js
@@ -1,13 +1,29 @@
-const myArray = [170, 45, 75, 90, 802, 24, 2, 66];
-
+const myArray = [170, 45, 75, 2, 802, 24, 2, 66];
 const radixSortArray = [[], [], [], [], [], [], [], [], [], []];
 
-function extractDigit(num, place) {
-  return String(num).split("").reverse()[place] || 0;
+function extractDigit(digit, idx) {
+  return Number(String(digit).split("").reverse()[idx]) || 0;
 }
 
-for (let i = 0; i < myArray.length; i++){
-	
+const max_value = Math.max(...myArray);
+let exp = 1;
+let trackedIndex = 1;
+
+while (Math.floor(max_value / exp) > 0) {
+  for (let i = 0; i < myArray.length; i++) {
+    radixSortArray[extractDigit(myArray[i], trackedIndex)].push(myArray[i]);
+  }
+
+  myArray.length = 0;
+
+  for (let j = 0; j < radixSortArray.length; j++) {
+    for (let k = 0; k < radixSortArray[j].length; k++) {
+      myArray.push(radixSortArray[j][k]);
+    }
+    radixSortArray[j].length = 0;
+  }
+  exp *= 10;
+  trackedIndex += 1;
 }
 
 console.log(myArray);
