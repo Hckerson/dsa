18044f2926df4de68425a61200ce7f883da81ad0
hckerson <hckerson@gmail.com>
2026-09-11 11:18:04 +0100

    Refactor linked list functions for clarity and add insertNodeAtPosition implementation

--- linked-list/single-linked-list.js
+++ linked-list/single-linked-list.js
@@ -8,7 +8,7 @@ class Node {
 function traverseAndPrint(node) {
     let currentNode = node;
     while (currentNode) {
-        console.log(`Current node data ${currentNode.data}`);
+        console.log(`Currently at ${currentNode.data}`)
         currentNode = currentNode.next;
     }
 }
@@ -26,23 +26,38 @@ function findLowestValue(head) {
 }
 
 function deleteSpecificNode(head, candidate) {
-    if (head === candidate) return head.next
+    if (head === candidate) return head.next;
 
-    let currentNode = head
-    if (currentNode.next && currentNode.next !== candidate) {
-        currentNode = currentNode.next
+    let currentNode = head;
+    while (currentNode.next && currentNode.next !== candidate) {
+        currentNode = currentNode.next;
     }
 
     if (!currentNode.next) {
-        return head
+        return head;
     }
 
-    currentNode.next = currentNode.next.next
-    return head
+    currentNode.next = currentNode.next.next;
+    return head;
 }
 
-function insertNodeAtPosition() {
-    
+function insertNodeAtPosition(head, newNode, position) {
+    if (position === 1) {
+        newNode.next = head;
+        return newNode;
+    }
+
+    let currentNode = head;
+
+    for (let i = 0; i < position - 2; i++) {
+        if (currentNode === null) break;
+        currentNode = currentNode.next;
+    }
+
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+
+    return head;
 }
 
 let node1 = new Node(7);
@@ -50,17 +65,18 @@ let node2 = new Node(11);
 let node3 = new Node(3);
 let node4 = new Node(2);
 let node5 = new Node(9);
+let node6 = new Node(10)
 
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
 node4.next = node5;
 
-console.log("Before deletion")
-traverseAndPrint(node1)
+traverseAndPrint(node1);
 
-node1 = deleteSpecificNode(node1, node1)
-console.log(`New ${node1.data}`)
+let start = insertNodeAtPosition(node1, node6, 3)
+traverseAndPrint(start)
 
-console.log("After deletion")
-traverseAndPrint(node1)
+start = deleteSpecificNode(node1, node1);
+
+traverseAndPrint(start);
