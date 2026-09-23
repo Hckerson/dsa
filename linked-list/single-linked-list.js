e16fd5c850adb87b85f5cc8669a5688f6ab577d8
Hckerson <166761968+Hckerson@users.noreply.github.com>
2026-09-23 09:07:23 +0100

    Fix formatting, improve readability, and correct logic in linked list functions

--- linked-list/single-linked-list.js
+++ linked-list/single-linked-list.js
@@ -8,42 +8,43 @@ class Node {
 function traverseAndPrint(node) {
     let currentNode = node;
     while (currentNode) {
-        console.log(`Currently at ${currentNode.data}`)
+        console.log(`Currently at ${currentNode.data}`);
         currentNode = currentNode.next;
     }
 }
 
 function findLowestValue(head) {
     let minValue = head.data;
-
     let currentNode = head.next;
+
     while (currentNode) {
-        const value = currentNode.data;
-        if (value < minValue) minValue = value;
+        data = currentNode.data;
+        if (data < minValue) {
+            minValue = data;
+        }
         currentNode = currentNode.next;
     }
-
     console.log(`Lowest  is Node with the value of ${minValue}`);
 }
 
 function deleteSpecificNode(head, candidate) {
-    if (head === candidate) return head.next;
+    if (head.data === candidate.data) return head.next;
 
     let currentNode = head;
-    while (currentNode.next && currentNode.next !== candidate) {
-        currentNode = currentNode.next;
+
+    while (currentNode.next && currentNode.next != candidate) {
+        currentNode.next;
     }
 
-    if (!currentNode.next) {
-        return head;
-    }
+    if (!currentNode.next) return head;
 
     currentNode.next = currentNode.next.next;
+
     return head;
 }
 
 function insertNodeAtPosition(head, newNode, position) {
-    if (position === 1) {
+    if (position == 1) {
         newNode.next = head;
         return newNode;
     }
@@ -51,12 +52,11 @@ function insertNodeAtPosition(head, newNode, position) {
     let currentNode = head;
 
     for (let i = 0; i < position - 2; i++) {
-        if (currentNode === null) break;
         currentNode = currentNode.next;
     }
 
-    newNode.next = currentNode.next;
-    currentNode.next = newNode;
+    newNode.next = currentNode.next
+    currentNode.next = newNode
 
     return head;
 }
@@ -66,7 +66,7 @@ let node2 = new Node(11);
 let node3 = new Node(3);
 let node4 = new Node(2);
 let node5 = new Node(9);
-let node6 = new Node(10)
+let node6 = new Node(10);
 
 node1.next = node2;
 node2.next = node3;
@@ -75,8 +75,8 @@ node4.next = node5;
 
 traverseAndPrint(node1);
 
-let start = insertNodeAtPosition(node1, node6, 3)
-traverseAndPrint(start)
+let start = insertNodeAtPosition(node1, node6, 3);
+traverseAndPrint(start);
 
 start = deleteSpecificNode(node1, node1);
 
